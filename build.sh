be6813c697b4fc4a215c35672af71d716132b773
Cédric Félizard <cedric@felizard.fr>
2018-10-07 06:38:22 +0000

    Don't hardcode path to box file

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ cd "$(dirname $0)"
 
 BOX=archlinux/archlinux
 PROVIDER=virtualbox
+VERSION=2018.10.05
+SOURCE_PATH=${HOME}/.vagrant.d/boxes/$(echo $BOX | sed 's/\//-VAGRANTSLASH-/g')/${VERSION}/virtualbox/box.ovf
 
 vagrant box update --provider $PROVIDER --box $BOX || vagrant box add --provider $PROVIDER $BOX
 vagrant box prune --name $BOX
-packer validate archlinux-chef.json
-packer build archlinux-chef.json
+packer validate -var "source_path=${SOURCE_PATH}" archlinux-chef.json
+packer build -var "source_path=${SOURCE_PATH}" archlinux-chef.json
 ls -lh packer_${PROVIDER}-ovf_${PROVIDER}.box
