223b6f67b30af6c234fdfc845648ea5d315c1e60
Cédric Félizard <cedric@felizard.fr>
2018-10-07 06:30:05 +0000

    Update packer binary name

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ PROVIDER=virtualbox
 
 vagrant box update --provider $PROVIDER --box $BOX || vagrant box add --provider $PROVIDER $BOX
 vagrant box prune --name $BOX
-packer-io validate archlinux-chef.json
-packer-io build archlinux-chef.json
+packer validate archlinux-chef.json
+packer build archlinux-chef.json
 ls -lh packer_${PROVIDER}-ovf_${PROVIDER}.box
