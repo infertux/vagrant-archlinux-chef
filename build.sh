d31debd985ad1c743800648112d5e9345098473a
Cédric Félizard <cedric@felizard.fr>
2018-12-18 09:07:20 +0000

    Allow to build images for libvirt provider

--- build.sh
+++ build.sh
@@ -1,14 +1,30 @@
-#!/bin/bash -eux
+#!/bin/bash
+
+set -euxo pipefail
 
 cd "$(dirname $0)"
 
+PROVIDER=${1:-libvirt}
+VERSION=${2:-2018.12.05}
+SOURCE_CHECKSUM=${3:-ad078771298fac16e4afdae8bfaa3b9a5bb27054327aa9307877d0f360c891d1}
+
+case $PROVIDER in
+    libvirt) EXTENSION=img;;
+    virtualbox) EXTENSION=ovf;;
+    *) echo >2 "Don't know file extension for ${PROVIDER}"; exit 1;;
+esac
+
 BOX=archlinux/archlinux
-PROVIDER=virtualbox
-VERSION=2018.10.05
-SOURCE_PATH=${HOME}/.vagrant.d/boxes/$(echo $BOX | sed 's/\//-VAGRANTSLASH-/g')/${VERSION}/virtualbox/box.ovf
+SOURCE_PATH=${HOME}/.vagrant.d/boxes/$(echo $BOX | sed 's/\//-VAGRANTSLASH-/g')/${VERSION}/${PROVIDER}/box.${EXTENSION}
 
 vagrant box update --provider $PROVIDER --box $BOX || vagrant box add --provider $PROVIDER $BOX
 vagrant box prune --name $BOX
-packer validate -var "source_path=${SOURCE_PATH}" archlinux-chef.json
-packer build -var "source_path=${SOURCE_PATH}" archlinux-chef.json
-ls -lh packer_${PROVIDER}-ovf_${PROVIDER}.box
+
+packer validate -var "source_path=${SOURCE_PATH}" \
+    -var "source_checksum=${SOURCE_CHECKSUM}" \
+    archlinux-chef-${PROVIDER}.json
+packer build -var "source_path=${SOURCE_PATH}" \
+    -var "source_checksum=${SOURCE_CHECKSUM}" \
+    archlinux-chef-${PROVIDER}.json
+
+ls -lh packer_*_${PROVIDER}.box
